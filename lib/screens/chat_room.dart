8d30aa307dc894894265d5709f18d9d73497ca0b
Yihun Alemayehu <yankure01@gmail.com>
2024-01-26 17:19:11 +0300

    fdb-06

--- lib/screens/chat_room.dart
+++ lib/screens/chat_room.dart
@@ -5,6 +5,14 @@ class ChatRoom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        leading: Row(
+          children: [
+            
+          ],
+        ),
+      )
+    );
   }
 }
