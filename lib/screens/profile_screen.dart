1d9cff950e46feb811385e49f309ab97cbc7c4ae
Yihun Alemayehu <yankure01@gmail.com>
2024-02-02 20:31:40 +0300

    fdb-08

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,6 +5,10 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+    );
   }
 }
