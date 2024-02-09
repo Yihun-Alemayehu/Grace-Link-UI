36d4a62a0e4cd190b8784b28d695dc8c99dee51f
Yihun Alemayehu <yankure01@gmail.com>
2024-02-09 11:10:51 +0300

    fdb-10

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -5,6 +5,11 @@ class PostScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a post'),
+      ),
+      body: const Column(),
+    );
   }
 }
